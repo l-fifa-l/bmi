961e46460edcefb842fa0148de8c99dfc9d706ef
Vivek <starktestic@gmail.com>
2022-03-21 18:42:35 +0530

    Initial commit

---
+++ lib/input_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  const InputPage({Key? key}) : super(key: key);
+
+  @override
+  State<InputPage> createState() => _InputPage();
+}
+
+class _InputPage extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('BMI Calculator'),
+      ),
+      body: const Center(
+        child: Text('Body Text'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
