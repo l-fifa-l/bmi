673902b028c4cd248acd9ae95c68334e082b96e9
Vivek <starktestic@gmail.com>
2022-03-23 12:47:27 +0530

    Update input_page.dart

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+const bottomContainerHeight = 80.0;
+const activeCardColor = Color(0xFF1D1E33);
+const bottomContainerColor = Color(0xFFEB1555);
+
 class InputPage extends StatefulWidget {
   const InputPage({Key? key}) : super(key: key);
 
@@ -11,16 +15,59 @@ class _InputPage extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('BMI Calculator'),
-      ),
-      body: const Center(
-        child: Text('Body Text'),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.add),
-      ),
+        appBar: AppBar(
+          title: const Text('BMI Calculator'),
+        ),
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: const <Widget>[
+                  Expanded(
+                    child: ReusableCard(colour: activeCardColor),
+                  ),
+                  Expanded(
+                    child: ReusableCard(colour: activeCardColor),
+                  )
+                ],
+              ),
+            ),
+            const Expanded(
+              child: ReusableCard(colour: activeCardColor),
+            ),
+            Expanded(
+              child: Row(
+                children: const <Widget>[
+                  Expanded(
+                    child: ReusableCard(colour: activeCardColor),
+                  ),
+                  Expanded(
+                    child: ReusableCard(colour: activeCardColor),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              color: bottomContainerColor,
+              margin: const EdgeInsets.only(top: 10.0),
+              width: double.infinity,
+              height: bottomContainerHeight,
+            )
+          ],
+        ));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  const ReusableCard({Key? key, required this.colour}) : super(key: key);
+
+  final Color colour;
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(15.0),
+      decoration: BoxDecoration(
+          color: colour, borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
